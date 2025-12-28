d5697746b1837e35f6fc93f506f9ddb47ebb7d84
YeoStrong <86854402+YeoStrong@users.noreply.github.com>
2025-12-28 18:36:45 +0100

    feat: add grid resizing and smooth fading effect

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
-container = document.querySelector("#container");
+const container = document.querySelector("#container");
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  const gridNum = prompt(
+    "Grid will be created by your number * number. What number do you want?(Max.100)"
+  );
+  if (0 < gridNum && gridNum <= 100) {
+    document.getElementById("container").innerHTML = "";
+    createGrid(gridNum);
+  } else {
+    alert("Please write number between 1 and 100");
+  }
+});
 
-for (i = 0; i < 16; i++) {
-  for (j = 0; j < 16; j++) {
-    const grid = document.createElement("div");
+function createGrid(size) {
+  const gridSize = 960 / size;
 
-    grid.addEventListener("mouseenter", () => {
-      const randomRed = Math.floor(Math.random() * 256);
-      const randomGreen = Math.floor(Math.random() * 256);
-      const randomBlue = Math.floor(Math.random() * 256);
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      const grid = document.createElement("div");
 
-      grid.style.backgroundColor = `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`;
-    });
+      grid.style.width = `${gridSize}px`;
+      grid.style.height = `${gridSize}px`;
+      grid.style.boxSizing = "border-box";
+      grid.style.border = "1px solid #eee";
 
-    grid.addEventListener("mouseleave", () => {
-      grid.style.backgroundColor = "white";
-    });
+      grid.addEventListener("mouseenter", () => {
+        grid.style.transition = "0s";
+        const randomRed = Math.floor(Math.random() * 256);
+        const randomGreen = Math.floor(Math.random() * 256);
+        const randomBlue = Math.floor(Math.random() * 256);
 
-    container.appendChild(grid);
+        grid.style.backgroundColor = `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`;
+      });
+
+      grid.addEventListener("mouseleave", () => {
+        grid.style.backgroundColor = "white";
+        grid.style.transition = "background-color 2s";
+      });
+
+      container.appendChild(grid);
+    }
   }
 }
+
+createGrid(16);
